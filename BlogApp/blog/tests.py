e1a92c57756356706b8ed1bdf416088233e0376b
Jonathan Bennett <jonathan@konoanalytics.com>
2019-10-22 14:50:17 -0500

    finished chapter 7 user accounts

--- BlogApp/blog/tests.py
+++ BlogApp/blog/tests.py
@@ -33,7 +33,7 @@ class BlogTests(TestCase):
         self.assertContains(response, 'Nice body content')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_post_detail_iew(self):
+    def test_post_detail_view(self):
         response = self.client.get('/post/1/')
         no_response = self.client.get('/post/100000/')
         self.assertEqual(response.status_code, 200)
